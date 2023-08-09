706cdb4d2a1f661c1be073cdd70b29a1a051f4b1
Cody Mitchell <comitche@redhat.com>
2023-08-09 13:34:44 -0400

    Wrap card in Link to add ctrl+click

--- src/pages/GalleryTemplate.tsx
+++ src/pages/GalleryTemplate.tsx
@@ -4,6 +4,7 @@ import {useNavigate} from "react-router";
 import {Gallery, GalleryItem} from "@patternfly/react-core";
 import {Card} from "../components/Card/Card";
 import {Tag, Tags} from "../components/Tags";
+import { Link } from "react-router-dom";
 
 interface GaleryProps {
     id?: string;
@@ -24,15 +25,23 @@ export const GalleryTemplate: FunctionComponent<GaleryProps> = ({id, elements, i
         <Gallery id={id} style={style} minWidths={{default: '300px'}} hasGutter>
             { elements.map(apiConfig => (
                 <GalleryItem key={apiConfig.displayName}>
-                    <Card apiId={apiConfig.id} displayName={apiConfig.displayName} icon={apiConfig.icon ?? APIConfigurationIcons.GenericIcon} description={apiConfig.description} onClick={() => navigate(pages.getApiPage(apiConfig.id))}>
-                        { apiConfig.tags.length > 0 && (
-                            <div className="apid-tags__main">
-                                <Tags>
-                                    {apiConfig.tags.map(t => <Tag key={t.id} value={t} />)}
-                                </Tags>
-                            </div>
-                        )}
-                    </Card>
+                    <Link to={pages.getApiPage(apiConfig.id)}>
+                        <Card
+                            apiId={apiConfig.id}
+                            displayName={apiConfig.displayName}
+                            icon={apiConfig.icon ?? APIConfigurationIcons.GenericIcon}
+                            description={apiConfig.description}
+                            onClick={() => {}}
+                        >
+                            { apiConfig.tags.length > 0 && (
+                                <div className="apid-tags__main">
+                                    <Tags>
+                                        {apiConfig.tags.map(t => <Tag key={t.id} value={t} />)}
+                                    </Tags>
+                                </div>
+                            )}
+                        </Card>
+                    </Link>
                 </GalleryItem>
             ))}
         </Gallery>
